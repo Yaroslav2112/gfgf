dd28db3afc4d70c81da0ba8e6c9502f666fe3647
yaroslav <zhjckfd21@ukr.net>
2018-10-13 14:04:18 +0300

    Initial commit

---
+++ src/main/java/class1.java
@@ -0,0 +1,12 @@
+public class class1 {
+    public static void main(String[] args) {
+        counter=0;
+        i=0;
+        while(counter!=10)
+            if(i>0;i%17==0)%%(i<0;i%6==0)
+        System.out.println(i);
+            i++;
+            counter++
+
+    }
+}
