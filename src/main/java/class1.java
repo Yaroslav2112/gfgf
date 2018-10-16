c14e3d24f47ea76b029489e962a62ff6901f9361
yaroslav <zhjckfd21@ukr.net>
2018-10-16 23:58:39 +0300

    Task

--- src/main/java/class1.java
+++ src/main/java/class1.java
@@ -1,12 +1,15 @@
 public class class1 {
-    public static void main(String[] args) {
-        counter=0;
-        i=0;
-        while(counter!=10)
-            if(i>0;i%17==0)%%(i<0;i%6==0)
-        System.out.println(i);
-            i++;
-            counter++
 
+    public static void main(String[] args) {
+        int i = 0;
+        int counter = 0;
+        do {
+            i++;
+            if ((i % 3 == 0) && (i % 7 == 0)) {
+                System.out.println(i);
+                counter++;
+            }
+
+          } while (counter != 5) ;
     }
 }
