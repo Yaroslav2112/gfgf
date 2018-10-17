e831a04e1d67c7c8592cb849633006b5601d5e89
yaroslav <zhjckfd21@ukr.net>
2018-10-17 21:39:10 +0300

    Task1

---
+++ src/main/java/Class2.java
@@ -0,0 +1,13 @@
+public class Class2 {
+
+
+    public static void main(String[] args) {
+        int i = 1;
+        System.out.println(i);
+        do {
+            i = i * 2;
+            System.out.println(i);
+        }
+    while(i!=512);
+}
+}
