1f2e0dc289176879c27628c28831f994ca6ea80d
yaroslav <zhjckfd21@ukr.net>
2018-10-19 21:39:02 +0300

    Task5

--- src/main/java/class5.java
+++ src/main/java/class5.java
@@ -1,10 +1,14 @@
 public class class5 {
     public static void main(String[] args) {
         int i=1;
-        int c=i-1;
         int counter=0;
-        do(i++);
-        if(i%c==0);
-
+        do {
+            i++;
+            if ((i == 2) || (i == 3) || (i == 5) || (i == 7))
+                System.out.println(i);
+            else if ((i % 2 != 0) && (i % 3 != 0) && (i % 5 != 0) && (i % 7 != 0))
+            { System.out.println(i);
+              counter++;}
+        } while (counter!=21);
     }
 }
